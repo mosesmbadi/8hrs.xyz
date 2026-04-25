781fc4218eb0e60c7cdfb9a32ccbc805292a3679
Moses Mbadi <mosesmbadi@gmail.com>
2026-04-25 21:06:33 +0300

    add allowedHosts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: true,
   css: ['~/assets/styles.css'],
-  modules: []
+  modules: [],
+  vite: {
+    server: {
+      allowedHosts: ['8hrs.xyz', 'www.8hrs.xyz', 'localhost']
+    }
+  }
 })
